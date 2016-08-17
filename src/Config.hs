dd22e374055a8f34f0043b545d6cab869795d559
codeonwort <alstjr5301@naver.com>
2016-08-17 18:06:14 +0900

    config added

---
+++ src/Config.hs
@@ -0,0 +1,5 @@
+module Config where
+
+uploader = "example.com/scrap/uploader.php"
+downloader = "example.com/scrap/download.php"
+remover = "example.com/scrap/remove.php"
---
+++ src/config.php
@@ -0,0 +1,7 @@
+<?php
+
+$db_id = "your_db_id";
+$db_pwd = "your_db_pwd";
+$db_name = "your_db_name";
+
+?>
